8182244d5614bd59627de6b85eb88cccd385609f
Anusha <ggelluanusha@gmail.com>
2026-01-06 21:31:39 +0530

    First commit

---
+++ chapter_3.py
@@ -0,0 +1,6 @@
+dict={"anu":"rehman",
+      "marks": 100,
+      "list": [0,1,2,3]
+      }
+anu=dict.clear()
+print(anu)
---
+++ python.py
@@ -0,0 +1,4 @@
+letter=''' Dear name
+you are selected
+date'''
+print(letter.replace("name",'rehman').replace("date","15/356/32"))
