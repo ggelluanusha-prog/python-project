e2bcfe144ce8819e9736fc3a5b21740086cfe86a
Gelu Anusha <ggelluanusha@gmail.com>
2026-01-06 22:47:29 +0530

    Update and rename python.py to string_replace.py

--- python.py
+++
@@ -1,4 +0,0 @@
-letter=''' Dear name
-you are selected
-date'''
-print(letter.replace("name",'rehman').replace("date","15/356/32"))
---
+++ string_replace.py
@@ -0,0 +1,6 @@
+letter = """Dear name,
+You are selected.
+Date: date
+"""
+
+print(letter.replace("name", "rehman").replace("date", "15/03/23"))
