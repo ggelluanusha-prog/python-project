3f22b5eb26b44ed48728da8d14bd05dd102996aa
Gelu Anusha <ggelluanusha@gmail.com>
2026-01-06 22:38:27 +0530

    Update and rename rehman.py to practice_basics.py

---
+++ practice_basics.py
@@ -0,0 +1,26 @@
+# Practice Programs in Python
+# 1. Print statement
+print("Welcome to Python Practice Programs")
+
+# 2. Variables
+name = "Anusha"
+age = 21
+print("Name:", name)
+print("Age:", age)
+
+# 3. Simple addition
+a = 10
+b = 20
+print("Sum:", a + b)
+
+# 4. Check even or odd
+num = 7
+if num % 2 == 0:
+    print(num, "is Even")
+else:
+    print(num, "is Odd")
+
+# 5. Loop example
+print("Numbers from 1 to 5:")
+for i in range(1, 6):
+    print(i)
